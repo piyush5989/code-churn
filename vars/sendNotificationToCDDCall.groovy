e445e35b83c9c04752e2183df3e7a2da68f7ae06
rally1-rallydev <74136437+rally1-rallydev@users.noreply.github.com>
2020-11-08 17:46:27 +0200

    Update sendNotificationToCDDCall.groovy

--- vars/sendNotificationToCDDCall.groovy
+++ vars/sendNotificationToCDDCall.groovy
@@ -1,32 +1,32 @@
 def call() {
  try {
   environmentSetUp()
-  sendNotificationToCDDirector()
-  processCDDReleases(evaluate("${currentBuild.description}"))
+  sendNotificationToDEVOPSirector()
+  processDEVOPSReleases(evaluate("${currentBuild.description}"))
  } catch (ex) {
-  echo "Exception occurred. Skipping notification to CDD. Error is [" + ex.toString() + "]"
+  echo "Exception occurred. Skipping notification to DEVOPS. Error is [" + ex.toString() + "]"
  }
 }
 
 void environmentSetUp() {
  getAPIKeyFromCredentials()
  setGitEnvironmentVariables()
- env.CDD_SERVER_NAME = "cddirector.io"
- env.CDD_SERVER_PORT = "443"
- env.CDD_TENANT_ID = "17827b55-3a42-403f-b1f8-9655d7e625bb"
- env.CDD_USE_SSL = true
- env.CDD_APPLICATION_NAME = "${env.GIT_URL.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')}"
- env.CDD_APPLICATION_VERSION = "$env.BRANCH_NAME"
- env.CDD_GIT_COMMIT_ID = "$env.GIT_COMMIT"
- env.CDD_PREVIOUS_GIT_COMMIT_ID = "$env.GIT_PREVIOUS_SUCCESSFUL_COMMIT"
+ env.DEVOPS_SERVER_NAME = "cddirector.io"
+ env.DEVOPS_SERVER_PORT = "443"
+ env.DEVOPS_TENANT_ID = "17827b55-3a42-403f-b1f8-9655d7e625bb"
+ env.DEVOPS_USE_SSL = true
+ env.DEVOPS_APPLICATION_NAME = "${env.GIT_URL.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')}"
+ env.DEVOPS_APPLICATION_VERSION = "$env.BRANCH_NAME"
+ env.DEVOPS_GIT_COMMIT_ID = "$env.GIT_COMMIT"
+ env.DEVOPS_PREVIOUS_GIT_COMMIT_ID = "$env.GIT_PREVIOUS_SUCCESSFUL_COMMIT"
 }
 
 void getAPIKeyFromCredentials() {
- def credentials = com.cloudbees.plugins.credentials.CredentialsProvider.findCredentialById('CDD_API_KEY',
+ def credentials = com.cloudbees.plugins.credentials.CredentialsProvider.findCredentialById('DEVOPS_API_KEY',
   com.cloudbees.plugins.credentials.Credentials.class,
   currentBuild.rawBuild, null
  );
- env.CDD_API_KEY = credentials.secret
+ env.DEVOPS_API_KEY = credentials.secret
 }
 
 void setGitEnvironmentVariables() {
@@ -49,34 +49,34 @@ String getLastSuccessfulCommit() {
  return lastSuccessfulHash
 }
 
-void sendNotificationToCDDirector() {
- echo '----------Sending Build Notification to CDD--------------'
- echo "Environment variables: GIT_URL: [$env.GIT_URL], GIT_BRANCH: [$env.GIT_BRANCH], BRANCH_NAME: [$env.BRANCH_NAME], GIT_LOCAL_BRANCH: [$env.GIT_LOCAL_BRANCH], CDD_APPLICATION_NAME: [${CDD_APPLICATION_NAME}], CDD_APPLICATION_VERSION: [${CDD_APPLICATION_VERSION}], GIT_COMMIT: [${env.GIT_COMMIT}], GIT_PREVIOUS_SUCCESSFUL_COMMIT: [${env.GIT_PREVIOUS_SUCCESSFUL_COMMIT}]"
- sendNotificationToCDD useSourceCodeRepositoryNameAsApplicationName: true,
-  appName: "${CDD_APPLICATION_NAME}",
+void sendNotificationToDEVOPSirector() {
+ echo '----------Sending Build Notification to DEVOPS--------------'
+ echo "Environment variables: GIT_URL: [$env.GIT_URL], GIT_BRANCH: [$env.GIT_BRANCH], BRANCH_NAME: [$env.BRANCH_NAME], GIT_LOCAL_BRANCH: [$env.GIT_LOCAL_BRANCH], DEVOPS_APPLICATION_NAME: [${DEVOPS_APPLICATION_NAME}], DEVOPS_APPLICATION_VERSION: [${DEVOPS_APPLICATION_VERSION}], GIT_COMMIT: [${env.GIT_COMMIT}], GIT_PREVIOUS_SUCCESSFUL_COMMIT: [${env.GIT_PREVIOUS_SUCCESSFUL_COMMIT}]"
+ sendNotificationToDEVOPS useSourceCodeRepositoryNameAsApplicationName: true,
+  appName: "${DEVOPS_APPLICATION_NAME}",
   useSourceCodeRepositoryBranchNameAsApplicationVersionName: true,
-  appVersion: "${CDD_APPLICATION_VERSION}",
-  gitCommit: "${CDD_GIT_COMMIT_ID}",
-  gitPrevSuccessfulCommit: "${CDD_PREVIOUS_GIT_COMMIT_ID}",
-  overrideCDDConfig: [
-   customApiKey: "${CDD_API_KEY}",
+  appVersion: "${DEVOPS_APPLICATION_VERSION}",
+  gitCommit: "${DEVOPS_GIT_COMMIT_ID}",
+  gitPrevSuccessfulCommit: "${DEVOPS_PREVIOUS_GIT_COMMIT_ID}",
+  overrideDEVOPSConfig: [
+   customApiKey: "${DEVOPS_API_KEY}",
    customProxyPassword: '',
    customProxyUrl: '',
    customProxyUsername: '',
-   customServerName: "${CDD_SERVER_NAME}",
-   customServerPort: "${CDD_SERVER_PORT}",
-   customTenantId: "${CDD_TENANT_ID}",
-   customUseSSL: "${CDD_USE_SSL}"
+   customServerName: "${DEVOPS_SERVER_NAME}",
+   customServerPort: "${DEVOPS_SERVER_PORT}",
+   customTenantId: "${DEVOPS_TENANT_ID}",
+   customUseSSL: "${DEVOPS_USE_SSL}"
   ],
   releaseTokens: '{}',
   ignoreNonexistentApplication: true
  echo '----------Jenkins Pipeline completed successfully--------------'
 }
 
-void processCDDReleases(Map cddReleaseMap) {
- if (cddReleaseMap) {
-  echo '----------Process CDD Releases--------------'
-  cddReleaseMap.eachWithIndex {
+void processDEVOPSReleases(Map devopsReleaseMap) {
+ if (devopsReleaseMap) {
+  echo '----------Process DEVOPS Releases--------------'
+  devopsReleaseMap.eachWithIndex {
    entry,
    index ->
    println "[$entry.key] = [$entry.value]"
